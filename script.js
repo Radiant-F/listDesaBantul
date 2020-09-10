2e2abae079f63632bf6305ccfc704e2e8b903958
RadiantF-pondok <71006921+RadiantF-pondok@users.noreply.github.com>
2020-09-10 17:18:30 +0700

    menambah index dan script
    tugas array nomor 1

---
+++ script.js
@@ -0,0 +1,17 @@
+var listDesaBantul = [
+    'Argodadi',
+    'Argomulyo',
+    'Argorejo',
+    'Argosari', [
+        'Bangunjiwo',
+        'Banguntapan',
+        'Bantul',
+        'Baturetno',
+        'Bawuran', [
+            'Canden',
+            'Caturharjo',
+            'Cetan',
+        ]
+    ]
+]
+document.write(listDesaBantul[4][5][1])
